2ef358f49c78d43952ba0efc68a9e1105af81674
Ardent-7322 <manishhpkumar34@gmail.com>
2024-11-02 14:19:45 +0530

    connect to MongoDB
    Signed-off-by: Ardent-7322 <manishhpkumar34@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,53 @@
+//require(`dotenv`).config({path: `./env`})
+
+import dotenv from "dotenv";
+import connectDB from "./DB/index.js";
+
+dotenv.config({
+    path: './env',
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Whenever try to connect Dabatabse always wrap them in try n catch or in Promises (error comes freqenntly ) and use async await (db always takes time to establish connection)
+
+/* Approch 1
+import express from "express"
+const app = express()
+
+(async() =>{
+    try{
+        await mongoose.connect (`${process.env.MONGOBD_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("App is not listning" ,error);
+            throw error
+        })
+
+        app.listen(process.env.PORT ,()=>{
+            console.log(`Server is running on port 
+                 ${process.env.PORT}`);
+
+        })
+    }catch (error){
+        console.log("Error while connecting to database", error)
+        throw err
+    }
+})()
+*/
