ccabe50468b64e8101ee5e17d22326690d352f63
Ardent-7322 <manishhpkumar34@gmail.com>
2024-11-03 01:08:41 +0530

    Models and Token
    Signed-off-by: Ardent-7322 <manishhpkumar34@gmail.com>

---
+++ src/models/Video.models.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //Url
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    Owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      index: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //Url
+      required:true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoSchema);
---
+++ src/models/user.models.js
@@ -0,0 +1,90 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"; //it helps in encryption of our password
+import jwt from "jsonwebtoken";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //Cloudinary url
+      required: true,
+    },
+    coverimage: {
+      type: String, //Cloudinary url
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    refreshTokens: {},
+  },
+  { timestamps: true },
+);
+
+//whenever i wil modified my password only then it wil execute, just before changes
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 12);
+  next();
+});
+
+//Now compare is our passwrod is correct or not
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefereshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+export const User = mongoose.model("User", userSchema);
